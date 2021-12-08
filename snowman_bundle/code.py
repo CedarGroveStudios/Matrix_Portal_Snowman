013549637906e6cc7881fc8d51bd76d7d0df381f
Cedar Grove Maker Studios <jan@goolsbey.net>
2021-12-07 18:51:30 -0800

    initial repo load

--- snowman_bundle/code.py
+++ snowman_bundle/code.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 
 # matrix_portal_snowman.py
-# 2021-12-07 v1.0
+# 2021-12-08 v1.0
 
 import board
 import displayio
@@ -23,11 +23,22 @@ display = matrix.display
 
 splash = displayio.Group()
 flakes = displayio.Group()
+
 display.show(splash)
 
 with open("/snowman_32_64.bmp", "rb") as f:
+
+    bkg_palette = displayio.Palette(4)
+
+    bkg_palette[0] = 0x000000
+    bkg_palette[1] = 0x600000
+    bkg_palette[2] = 0x000060
+    bkg_palette[3] = 0x606060
+
+    bkg_palette.make_transparent(0)
+
     background = displayio.OnDiskBitmap(f)
-    background = displayio.TileGrid(background, pixel_shader=displayio.ColorConverter(), x=-6)
+    background = displayio.TileGrid(background, pixel_shader=bkg_palette, x=-6)
     splash.append(background)
 
     if max_flakes == 0:
@@ -35,7 +46,7 @@ with open("/snowman_32_64.bmp", "rb") as f:
             pass
 
     for i in range(0, max_flakes):
-        snowflake = Circle(rn.randrange(0, 68), i % 32, 1, fill=0x8080ff)
+        snowflake = Circle(rn.randrange(0, 68), i % 32, 1, fill=0xffffff)
         flakes.append(snowflake)
         flake_time.append(rn.random() / max_flakes)
         flake_displ.append(rn.randrange(1,3))
